bbbe33e96ee889e40c771ec13690790e7b9deb94
Paul Milovanov <paul@dada.science>
2018-09-07 04:19:18 -0400

    Caching for processed training data and the embedding matrix (optimized for the vocabulary used). Also now showing default values in --help

--- src/artstat/models/onehot.py
+++ src/artstat/models/onehot.py
@@ -1,5 +1,7 @@
 import math
 import os.path
+import pathlib
+import pickle
 import sys
 import textwrap
 
@@ -88,47 +90,56 @@ def make_predict_model(model_train):
 
 ######################################################################################################################
 file_path = click.Path(exists=True, dir_okay=False, resolve_path=True)
-write_file_path = click.Path(exists=True, dir_okay=False, resolve_path=True, writable=True)
+write_file_path = click.Path(exists=False, dir_okay=False, resolve_path=True, writable=True)
 dir_path = click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True)
+write_dir_path = click.Path(exists=False, file_okay=False, dir_okay=True, resolve_path=True)
 
 
-@click.group()
+def opt(*args, **kwargs):
+    return click.option(*args, show_default=True, **kwargs)
+
+
+ctx_settings = dict(terminal_width=120, color=True)
+
+
+@click.group(context_settings=ctx_settings)
 def main():
     pass
 
 
 def flags_resources(f):
-    f = option("--glove_file", required=True, type=file_path,
-               help="File containing pretrained GloVe word embedding data, e.g one of the files from "
-                    "http://nlp.stanford.edu/data/glove.6B.zip")(f)
-    f = option("--glove_dims", required=True, type=int,
-               help="Dimensionality of GloVe word embedding output vectors. Must match that in the provided "
-                    "<glove_file>, e.g 300 for glove.6B.300d.txt")(f)
+    f = opt("--glove_file", required=True, type=file_path,
+            help="File containing pretrained GloVe word embedding data, e.g one of the files from "
+                 "http://nlp.stanford.edu/data/glove.6B.zip")(f)
+    f = opt("--glove_dims", required=True, type=int,
+            help="Dimensionality of GloVe word embedding output vectors. Must match that in the provided "
+                 "<glove_file>, e.g 300 for glove.6B.300d.txt")(f)
     return f
 
 
 def flags_shared(f):
-    f = option("--seqlen", default=64, help="Length of input sequence of words to predict from.")(f)
-    f = option("--vocab_size", default=10000, help="Only use this many words from the top of the vocabulary file.")(f)
-    f = option("--vocab_file", required=True, type=file_path,
-               help="Vocabulary file to use to map words to integer ids.")(f)
-    f = option("--vocab_is_lowercase", default=True,
-               help="If true, will convert the word to lowercase before vocabulary lookup. True by default.")(f)
+    f = opt("--seqlen", default=64, help="Length of input sequence of words to predict from.")(f)
+    f = opt("--vocab_size", default=10000, help="Only use this many words from the top of the vocabulary file.")(f)
+    f = opt("--vocab_file", required=True, type=file_path,
+            help="Vocabulary file to use to map words to integer ids.")(f)
+    f = opt("--vocab_is_lowercase", default=True,
+            help="If true, will convert the word to lowercase before vocabulary lookup. True by default.")(f)
     return f
 
 
 def flags_hyperparams(f):
-    f = option("--lstm_size", type=int, default=256)(f)
-    f = option("--lstm_layers", type=int, default=2)(f)
-    f = option("--dense_size", type=int, default=256)(f)
-    f = option("--dense_layers", type=int, default=5)(f)
-    f = option("--dropout_rate", type=float, default=0.1)(f)
-    f = option("--learning_rate_initial", type=float, default=0.01)(f)
-    f = option("--learning_rate_decay_rate", type=float, default=0.97)(f)
-    f = option("--learning_rate_decay_period", type=int, default=10)(f)
-    f = option("--batch_size", type=int, default=128)(f)
-    f = option("--sample_size", default=10,
-               help=("Sample size to be used for negative sampling. Includes the positive example."))(f)
+    f = opt("--lstm_size", type=int, default=256)(f)
+    f = opt("--lstm_layers", type=int, default=2)(f)
+    f = opt("--dense_size", type=int, default=256)(f)
+    f = opt("--dense_layers", type=int, default=5)(f)
+    f = opt("--dropout_rate", type=float, default=0.1)(f)
+    f = opt("--learning_rate_initial", type=float, default=0.01)(f)
+    f = opt("--learning_rate_decay_rate", type=float, default=0.97)(f)
+    f = opt("--learning_rate_decay_period", type=int, default=10)(f)
+    f = opt("--learning_rate_floor", type=float, default=1E-10)(f)
+    f = opt("--batch_size", type=int, default=128)(f)
+    f = opt("--sample_size", default=10,
+            help=("Sample size to be used for negative sampling. Includes the positive example."))(f)
     return f
 
 
@@ -141,18 +152,22 @@ def info(*args):
 @flags_resources
 @flags_shared
 @flags_hyperparams
-@option("--checkpoint_dir", required=True, type=dir_path)
-@option("--starting_model_file", type=file_path)
-@option("--training_data_dir", required=True, type=dir_path,
-        help="Dir containing training data as text files. All files under this dir will be read recursively.")
-@option("--training_max_files", default=0)
-@option("--num_epochs", default=5, help="Train for this many epochs")
-@option("--starting_epoch", default=0)
-@option("--epochs_per_dataset", default=32)
+@opt("--checkpoint_dir", required=True, type=dir_path)
+@opt("--starting_model_file", type=file_path)
+@opt("--training_data_dir", required=True, type=dir_path,
+     help="Dir containing training data as text files. All files under this dir will be read recursively.")
+@opt("--training_max_files", default=0)
+@opt("--num_epochs", default=5, help="Train for this many epochs")
+@opt("--starting_epoch", default=0)
+@opt("--epochs_per_dataset", default=32)
+@opt("--cache_dir", default="/tmp/cache/artstat/onehot", type=write_dir_path)
 def train(vocab_file, vocab_is_lowercase, glove_file, glove_dims, training_data_dir, training_max_files, checkpoint_dir,
           starting_model_file, seqlen, vocab_size, lstm_size, lstm_layers, dense_size, dense_layers, dropout_rate,
-          sample_size, learning_rate_initial, learning_rate_decay_rate, learning_rate_decay_period, batch_size,
-          num_epochs, starting_epoch, epochs_per_dataset):
+          sample_size, learning_rate_initial, learning_rate_decay_rate, learning_rate_decay_period,
+          learning_rate_floor, batch_size, num_epochs, starting_epoch, epochs_per_dataset, cache_dir):
+    if cache_dir:
+        pathlib.Path(cache_dir).mkdir(parents=True, exist_ok=True)
+
     info("Loading vocabulary from ", vocab_file)
     words, vocab = util.load_vocab(vocab_file, vocab_size)
     info("Loaded", len(vocab), "words")
@@ -162,16 +177,39 @@ def train(vocab_file, vocab_is_lowercase, glove_file, glove_dims, training_data_
         info("Loading model from ", starting_model_file)
         model = keras.models.load_model(starting_model_file)
     else:
-        info("Loading embedding matrix")
-        emb_matrix = util.load_embeddings(vocab, glove_dims, glove_file)
+
+        emb_matrix_cache_path = os.path.join(cache_dir, "emb_matrix")
+        if cache_dir and os.path.exists(emb_matrix_cache_path):
+            info("Loading embedding matrix from cache")
+            with open(emb_matrix_cache_path, "rb") as f:
+                emb_matrix = pickle.Unpickler(f).load()
+        else:
+            info("Loading embedding matrix")
+            emb_matrix = util.load_embeddings(vocab, glove_dims, glove_file)
+            if cache_dir:
+                info("Writing embedding matrix cache")
+                with open(emb_matrix_cache_path, "wb") as f:
+                    pickle.Pickler(f, protocol=pickle.HIGHEST_PROTOCOL).dump(emb_matrix)
         info("Creating model")
         model = make_model(emb_matrix=emb_matrix, vocab=vocab, seqlen=seqlen, sample_size=sample_size,
                            lstm_size=lstm_size, dense_size=dense_size, dense_layers=dense_layers,
                            lstm_layers=lstm_layers, dropout_rate=dropout_rate)
 
-    info("Loading training data from", training_data_dir)
-    X, Xu = util.load_data(training_data_dir, vocab, pad=seqlen, numfiles=training_max_files,
+    training_data_cache_path = os.path.join(cache_dir, "training_data")
+    if cache_dir and os.path.exists(training_data_cache_path):
+        info("Loading training data from cache:", training_data_cache_path)
+        with open(training_data_cache_path, "rb") as f:
+            unpickler = pickle.Unpickler(f)
+            X, Xu = unpickler.load()
+    else:
+        info("Loading training data from", training_data_dir)
+        X, Xu = util.load_data(training_data_dir, vocab, pad=seqlen, numfiles=training_max_files,
                            lowercase=vocab_is_lowercase)
+        if cache_dir:
+            info("Saving prepared training data to cache:", training_data_cache_path)
+            with open(training_data_cache_path, "wb") as f:
+                pickler = pickle.Pickler(f, pickle.HIGHEST_PROTOCOL)
+                pickler.dump([X, Xu])
 
     info("Unknown words in training data: %.2f%%" % util.unknown_word_percentage(Xu))
 
@@ -182,8 +220,10 @@ def train(vocab_file, vocab_is_lowercase, glove_file, glove_dims, training_data_
     checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_filepath, verbose=2, save_best_only=True, monitor='loss')
 
     def decay(epoch):
-        return learning_rate_initial * math.pow(learning_rate_decay_rate,
-                                                math.floor(epoch / learning_rate_decay_period))
+        lr = learning_rate_initial * math.pow(learning_rate_decay_rate,
+                                              math.floor(epoch / learning_rate_decay_period))
+        lr = max(lr, learning_rate_floor)
+        return lr
 
     decay_scheduler = LearningRateScheduler(decay, verbose=1)
 
@@ -202,9 +242,9 @@ def train(vocab_file, vocab_is_lowercase, glove_file, glove_dims, training_data_
 #######################################################################################################################
 @main.command('sample')
 @flags_shared
-@option("--model_file", type=file_path)
-@option("--num_words_to_sample", default=5)
-@option("--init_text", prompt="Initialization text", type=str)
+@opt("--model_file", type=file_path)
+@opt("--num_words_to_sample", default=5)
+@opt("--init_text", prompt="Initialization text", type=str)
 def sample(vocab_file, vocab_is_lowercase, seqlen, vocab_size, model_file, num_words_to_sample, init_text):
     cols = 80
     info("Loading vocabulary")
--- src/artstat/models/sample_onehot.sh
+++ src/artstat/models/sample_onehot.sh
@@ -2,9 +2,9 @@
 
 python3 /app/src/artstat/models/onehot.py sample --vocab_file=vocab_lower.txt \
     --vocab_is_lowercase=True \
-    --seqlen=64 \
+    --seqlen=32 \
     --vocab_size=10000 \
-    --model_file=/app/notebooks/checkpoints/m2//weights.lstm1024.batch128.glove300.sample10.vocab10000.default.hdf5 \
+    --model_file=/app/notebooks/checkpoints/m2/weights.lstm128.batch128.glove300.sample10.vocab10000.default.hdf5 \
     --num_words_to_sample=100 \
     --init_text="Mary had a little simulacrum, its exterior attributes white and cold. And everywhere that Mary went
     the simulacrum would also go."
