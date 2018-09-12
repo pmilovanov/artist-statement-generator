f537ec871a0a1436f3e452d56857f9e6ccaf1a64
Paul Milovanov <paul@dada.science>
2018-09-11 22:56:35 -0400

    shuffled around dockerfiles to allow for building on google cloud build

--- docker-build.sh
+++ docker-build.sh
@@ -9,9 +9,9 @@ echo "where <docker-image-name> has a corresponding *.Dockerfile in docker/"
 exit 1
 fi
 
-df="docker/$IMAGE.Dockerfile"
+df="docker/$IMAGE/Dockerfile"
 if [[ -f "$df" ]]; then
-  nvidia-docker build . -f "$df" -t "$IMAGE"
+  nvidia-docker build docker/$IMAGE -t "$IMAGE"
   exit 0
 fi
 
